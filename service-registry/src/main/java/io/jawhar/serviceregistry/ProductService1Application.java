aed0c6a12babebfee631f4d495cdaf4aecbf50bc
jawher khelil <45321598+kheliljawher@users.noreply.github.com>
2021-12-28 08:48:31 +0100

    service registry with eureka server

---
+++ service-registry/src/main/java/io/jawhar/serviceregistry/ProductService1Application.java
@@ -0,0 +1,15 @@
+package io.jawhar.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ProductService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductService1Application.class, args);
+	}
+
+}
